ffb8f0e0bde75ca981e51d322eaa1ded15b23707
AlejaArteaga1 <alejaarteaga17@hotmail.com>
2024-11-10 09:15:16 -0500

    Update Homework_E_0.py

--- Chapter1_Programing_Basic/Class1-3(8)_Lists/Homework_E_0.py
+++ Chapter1_Programing_Basic/Class1-3(8)_Lists/Homework_E_0.py
@@ -1,5 +1,4 @@
 ## Exercise 0
-
 # Please create a function to create a list to 1 to max_number
 
 """ 
@@ -9,6 +8,10 @@ n = 10
 Output
 
 [1,2,3,4,5,6,7,8,9,10] """
-
+n = 25
 def create_counter_list(max_number):
-    pass # Your code here (Delete this line)
+    list =[]
+    for i in range(1, max_number + 1):
+        list.append(i)
+    return list
+print(create_counter_list(n))
